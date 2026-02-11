09875de7a16a3a7fbb3429da6bb8f7d373dd75fb
AGonzales-amd <angonzal@amd.com>
2026-02-11 23:07:44 +0000

    tuned potrf blk size

--- projects/rocsolver/library/src/include/ideal_sizes.hpp
+++ projects/rocsolver/library/src/include/ideal_sizes.hpp
@@ -278,22 +278,15 @@
 
 /************************** potf2/potrf ***************************************
 *******************************************************************************/
-/*! \brief Determines the size of the leading block that is factorized at each step
-    when using the blocked algorithm (POTRF). It also applies to the
-    corresponding batched and strided-batched routines.*/
-#ifndef POTRF_BLOCKSIZE
-#define POTRF_BLOCKSIZE(T) ((sizeof(T) == 4) ? 256 : (sizeof(T) == 8) ? 128 : 64)
-#endif
-
 /*! \brief Determines the size at which rocSOLVER switches from
     the unblocked to the blocked algorithm when executing POTRF. It also applies to the
     corresponding batched and strided-batched routines.
 
-    \details POTRF will factorize blocks of POTRF_BLOCKSIZE columns at a time until
+    \details POTRF will factorize blocks of columns at a time until
     the rest of the matrix has no more than POTRF_POTF2_SWITCHSIZE columns; at this point the last block,
     if any, will be factorized with the unblocked algorithm (POTF2).*/
 #ifndef POTRF_POTF2_SWITCHSIZE
-#define POTRF_POTF2_SWITCHSIZE(T) POTRF_BLOCKSIZE(T)
+#define POTRF_POTF2_SWITCHSIZE(T) 64
 #endif
 
 /*! \brief Determines the maximum size at which rocSOLVER can use POTF2
@@ -302,7 +295,7 @@
     within the LDS share memory using compact storage.
     The amount of LDS shared memory is assumed to be at least (64 * 1024) bytes. */
 #ifndef POTF2_MAX_SMALL_SIZE
-#define POTF2_MAX_SMALL_SIZE(T) ((sizeof(T) == 4) ? 256 : (sizeof(T) == 8) ? 128 : 64)
+#define POTF2_MAX_SMALL_SIZE(T) ((sizeof(T) == 16) ? 128 : 256)
 #endif
 
 /************************** syevj/heevj ***************************************
